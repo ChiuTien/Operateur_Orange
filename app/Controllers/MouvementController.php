051d73eef828159640f147be366d83e52b56370e
ChiuTien <chiumandresychristian@gmail.com>
2026-07-20 11:07:18 +0300

    Go

--- app/Controllers/MouvementController.php
+++ app/Controllers/MouvementController.php
@@ -26,18 +26,30 @@ class MouvementController extends BaseController {
         return redirect()->to("/accueil");
     }
 
-    public function retrait() {
+    public function retrait($idNum, $montant) {
         $mouvement = new Mouvement();
+
+        $soldeData = $this->getSolde($idNum);
+        $solde = $soldeData['solde'] ?? 0.0;
+
+        if($solde >= $montant) {
+            $donnee = [
+                "idN1" => $idNum,
+                "idOperation" => 2,
+                "argent" => $montant
+            ];
+        }
     }
 
-    public function getSolde($idNumero) {
+    public function getSolde($idNum) {
         $resultat = $model->select("
             SUM(
                 CASE 
                     WHEN idOperation IN (1, 2) THEN -argent
                     ELSE argent                              
                 END
-            ) AS solde_total
-        ")->where('idN1', $numeroN1)->first();
+            ) AS solde
+        ")->where('idN1', $idNum)->first();
+        return $resultat;
     }
 }

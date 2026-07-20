47d2bf71db396d195842606055deb400c73b24b7
VlexandreMG <dodolashmou@gmail.com>
2026-07-20 15:31:17 +0300

    Page de choix operateur sy client

---
+++ app/Views/loginMain.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Title</title>
+</head>
+<body>
+    <h2>Voulez_vous vous connectez en quel rôles</h2>
+    <a href="/login">Client</a>  
+    <a href="/operator">Operateur</a>  
+</body>
+</html>

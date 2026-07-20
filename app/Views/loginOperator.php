23f4adb0518a9dfde433ca092a4fe62cc3db0fa4
VlexandreMG <dodolashmou@gmail.com>
2026-07-20 15:22:09 +0300

    loginOperator

---
+++ app/Views/loginOperator.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login operateur</title>
+</head>
+<body>
+    <h2>Veuillez entrer vos identifiants : </h2><br>
+    <form action="" method="post">
+        <label for="nom">Entrer votre nom</label><br>
+        <input type="text" name="nom" placeholder="Yas" required><br>
+        <label for="mdp">Entrer votre mot de passe</label><br>
+        <input type="text" name="mdp" required><br>
+        <button type="submit">Connexion</button>
+    </form>
+</body>
+</html>

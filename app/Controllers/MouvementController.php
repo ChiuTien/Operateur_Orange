5981d7f9f1ae3d81e15cb4de102295b5ced1ea6c
ChiuTien <chiumandresychristian@gmail.com>
2026-07-20 10:05:37 +0300

    Alefa !

--- app/Controllers/MouvementController.php
+++ app/Controllers/MouvementController.php
@@ -1,3 +1,14 @@
-<?php 
+<?php
 
-?>
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class MouvementController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}

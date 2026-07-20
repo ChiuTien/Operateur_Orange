3209508fe19d6cbdb59afba8b7b13ac35a89e287
VlexandreMG <dodolashmou@gmail.com>
2026-07-20 09:42:30 +0300

    accueil page vita

--- Taches.md
+++ Taches.md
@@ -5,8 +5,8 @@
 - Interface & Userflow [Alexandre]
   - Userflow: login [ok]
   - Userflow: accueil [ok]
-  - Interface: login
-  - Interface: accueil
+  - Interface: login [ok]
+  - Interface: accueil 
   - Controllers: OprationController
   - Routes 
 - MCD & Back
---
+++ app/Views/acceuil.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceuil</title>
+</head>
+<body>
+    <h2>C'est un plaisir de vous revoir.</h2>
+    <!-- Voir solde -->
+
+    <!-- Faire un depot -->
+    <h3>Combien allez vous versez ?</h3>
+    <form action="" method="post">
+        <label for="montant">Le montant à verser</label><br>
+        <input type="number" placeholder="15000"><br>
+        <button type="submit"> Valider le depot </button>
+    </form>
+
+    <!-- Faire un retrait -->
+
+    <!-- Faire un transfert -->
+
+    <!-- Voir historique -->
+</body>
+</html>

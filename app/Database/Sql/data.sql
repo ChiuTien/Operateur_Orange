f7c2ff1271a9f58f70f40e7f39a3071746f57509
ChiuTien <chiumandresychristian@gmail.com>
2026-07-20 10:58:44 +0300

    Donnee de test

---
+++ app/Database/Sql/data.sql
@@ -0,0 +1,4 @@
+-- Active: 1784529240551@@127.0.0.1@3306
+INSERT INTO operation(id,libelle) VALUES(1,'Depot');
+INSERT INTO operation(id,libelle) VALUES(2,'Retrait');
+INSERT INTO operation(id,libelle) VALUES(3,'Transfert');

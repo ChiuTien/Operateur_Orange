4cfc2f4b524aa81fa76204de6867d4b8bbd9509f
VlexandreMG <dodolashmou@gmail.com>
2026-07-20 10:46:56 +0300

    Au moins mande le izy

--- app/Views/login.php
+++ app/Views/login.php
@@ -7,6 +7,12 @@
 </head>
 <body>
     <h2>Veuillez taper vos identifiants : </h2>
+    <!-- Affichage du message d'erreur Flashdata -->
+    <?php if (session()->getFlashdata('error')): ?>
+        <div style="color: red; margin-bottom: 15px; font-weight: bold;">
+            <?= session()->getFlashdata('error') ?>
+        </div>
+    <?php endif; ?>
     <form action="login/auth" method="post">
         <label for="numero">Taper votre numero</label><br>
         <input type="text" name="numero" required><br>

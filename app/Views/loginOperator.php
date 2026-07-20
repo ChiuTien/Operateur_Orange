bf178d454d9b6c5d74454dba6c1aa76c619149d0
VlexandreMG <dodolashmou@gmail.com>
2026-07-20 15:42:02 +0300

    Détails kely @ le logique ana operateur

--- app/Views/loginOperator.php
+++ app/Views/loginOperator.php
@@ -157,15 +157,17 @@
         <h2>Connexion operateur</h2>
         <div class="sous-titre">Veuillez entrer vos identifiants</div>
 
-        <form action="" method="post">
+        <form action="login/authOpe" method="post">
             <div class="form-group">
                 <label for="nom">Entrer votre nom</label>
                 <input type="text" id="nom" name="nom" placeholder="Yas" required>
+                <small>Ex : Yas</small>
             </div>
 
             <div class="form-group">
                 <label for="mdp">Entrer votre mot de passe</label>
                 <input type="password" id="mdp" name="mdp" placeholder="Votre mot de passe" required>
+                <small>Ex : 123</small>
             </div>
 
             <button type="submit">Connexion</button>

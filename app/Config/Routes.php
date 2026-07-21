7659fb84133f733871aafe1c8d64cb824b89901f
VlexandreMG <dodolashmou@gmail.com>
2026-07-21 05:06:34 +0300

    routes sur les prefixes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,8 +12,15 @@ $routes->get('/client', 'Home::client');
 $routes->get('/accueil', 'Home::accueil');
 $routes->get('/operator', 'Home::operator');
 $routes->get('/operation', 'Home::listOperation');
-$routes->get('/prefixe', 'OperateurController::listPrefixe');
 
+// Préfixes 
+$routes->get('/prefixe', 'OperateurController::listPrefixe');
+$routes->get('prefixe/ajouter', 'OperateurController::formAddPrefixe');
+$routes->post('prefixe/create', 'OperateurController::createPrefixe');
+$routes->get('prefixe/delete/(:num)', 'OperateurController::deletePrefixe/$1');
+
+// (Optionnel pour plus tard) Afficher la page de modification
+$routes->get('prefixe/edit/(:num)', 'OperateurController::formEditPrefixe/$1');
 
 $routes->post('/login/auth', 'LoginController::auth');
 $routes->post('/login/authOpe', 'LoginController::authOpe');

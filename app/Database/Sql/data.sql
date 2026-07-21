10be887fdf52de9e2dd03978a6a53578204c789a
VlexandreMG <dodolashmou@gmail.com>
2026-07-21 08:58:22 +0300

    Nampiditra données

--- app/Database/Sql/data.sql
+++ app/Database/Sql/data.sql
@@ -1,4 +1,4 @@
--- Active: 1784529240551@@127.0.0.1@3306
+-- Active: 1784529228960@@127.0.0.1@3306
 INSERT INTO operation(id,libelle) VALUES(1,'Depot');
 INSERT INTO operation(id,libelle) VALUES(2,'Retrait');
 INSERT INTO operation(id,libelle) VALUES(3,'Transfert');
